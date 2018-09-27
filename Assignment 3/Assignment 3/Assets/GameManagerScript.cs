47a84c2ad81855c4717ee117691e47ec135a26b5
16342613 <43452498+16342613@users.noreply.github.com>
2018-09-27 12:47:48 +0100

    A2

---
+++ Assignment 3/Assignment 3/Assets/GameManagerScript.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour {
+
+    public GameObject mars;
+    public GameObject phobos;
+    public GameObject deimos;
+
+    // Use this for initialization
+    void Start () {
+        Camera.main.transform.position = new Vector3(0f, 0f, -130f);
+        Camera.main.transform.LookAt(mars.transform);
+        mars.GetComponent<Rigidbody>().AddTorque(new Vector3(0f, 20f, 0f));
+        phobos.GetComponent<Rigidbody>().AddTorque(new Vector3(0f, 20f, 0f));
+        deimos.GetComponent<Rigidbody>().AddTorque(new Vector3(0f, 20f, 0f));
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        phobos.transform.RotateAround(Vector3.zero, Vector3.up, 25 * Time.deltaTime);
+        deimos.transform.RotateAround(Vector3.zero, Vector3.up, 20 * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftArrow)) {
+            Camera.main.transform.RotateAround(Vector3.zero, Vector3.up, 30*Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow)) {
+            Camera.main.transform.RotateAround(Vector3.zero, Vector3.down, 30 * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow)) { 
+            Camera.main.transform.RotateAround(Vector3.zero, Vector3.right, 30 * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.DownArrow)) {
+            Camera.main.transform.RotateAround(Vector3.zero, Vector3.left, 30 * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Space)) {
+            Camera.main.transform.position = new Vector3(0f, 0f, -130f);
+            Camera.main.transform.LookAt(mars.transform);
+        }
+    }
+}
