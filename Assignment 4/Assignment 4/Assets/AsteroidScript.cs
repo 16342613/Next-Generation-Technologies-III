772e683d9264ff0667f148a25925b911345962e9
16342613 <43452498+16342613@users.noreply.github.com>
2018-10-14 12:34:17 +0100

    ss

---
+++ Assignment 4/Assignment 4/Assets/AsteroidScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AsteroidScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<Rigidbody>().AddForce(new Vector3(Random.Range(-100, 100), 0, Random.Range(-100, 100)));  // Apply random force
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        InvokeRepeating("CheckOutOfBounds", 0f, 0.2f);  // Check bounds 5x per second
+	}
+
+    void CheckOutOfBounds() {
+        if ((this.transform.position.x > 30) || (this.transform.position.x < -30))
+        {
+            Vector3 lastKnownPosition = this.transform.position;
+            this.transform.position = new Vector3(-lastKnownPosition.x, lastKnownPosition.y, lastKnownPosition.z);  // Reverse x
+        }
+        else if ((this.transform.position.z > 18) || (this.transform.position.z < -18))
+        {
+            Vector3 lastKnownPosition = this.transform.position;
+            this.transform.position = new Vector3(lastKnownPosition.x, lastKnownPosition.y, -lastKnownPosition.z);  // Reverse z
+        }
+    }
+}
---
+++ Assignment 4/Assignment 4/Assets/GameManagerScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerScript : MonoBehaviour
+{
+
+    public int currentGameLevel;
+    public GameObject asteroid;
+
+    // Use this for initialization
+    void Start()
+    {
+        Camera.main.transform.position = new Vector3(0f, 30f, 0f);
+        Camera.main.transform.LookAt(Vector3.zero);
+        StartGameLevel();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void StartGameLevel()
+    {
+        // Spawn 2 asteroids on each edge
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                GameObject.Instantiate(asteroid);
+                if (i == 0)
+                {
+                    asteroid.transform.position = new Vector3(Random.Range(-28, 28), 0, 15);
+                }
+                else if (i == 1)
+                {
+                    asteroid.transform.position = new Vector3(28, 0, Random.Range(-15, 15));
+                }
+                else if (i == 2)
+                {
+                    asteroid.transform.position = new Vector3(Random.Range(-28, 28), 0, -15);
+                }
+                else if (i == 3)
+                {
+                    asteroid.transform.position = new Vector3(-28, 0, Random.Range(-15, 15));
+                }
+            }
+        }
+    }
+}
