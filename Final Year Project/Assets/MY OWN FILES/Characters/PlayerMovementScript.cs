ca77e8c76e70030d2a3079f0bd9f44c089add780
16342613 <43452498+16342613@users.noreply.github.com>
2018-10-20 23:42:36 +0100

    update

--- Final Year Project/Assets/MY OWN FILES/Characters/PlayerMovementScript.cs
+++ Final Year Project/Assets/MY OWN FILES/Characters/PlayerMovementScript.cs
@@ -6,12 +6,20 @@ public class PlayerMovementScript : MonoBehaviour
 {
 
     Animator mAnim;
+    public float mouseSensitivity = 100.0f;
+    public float clampAngle = 80.0f;
+
+    private float rotY = 0.0f; // rotation around the up/y axis
+    private float rotX = 0.0f; // rotation around the right/x axis
 
 
     // Use this for initialization
     void Start()
     {
         mAnim = GameObject.FindWithTag("Player").GetComponent<Animator>();
+        Vector3 rot = transform.localRotation.eulerAngles;
+        rotY = rot.y;
+        rotX = rot.x;
     }
 
     // Update is called once per frame
@@ -27,7 +35,7 @@ public class PlayerMovementScript : MonoBehaviour
         //{
            //Debug.Log("Value Returned: " + leftStickHorizontal.ToString("F2"));
         //}
-
+/*
         if (leftStickVertical > 0.15 && leftStickHorizontal < 0.15)
         {
             if (Input.GetKey(KeyCode.JoystickButton10))
@@ -54,7 +62,7 @@ public class PlayerMovementScript : MonoBehaviour
             mAnim.SetTrigger("Strafe Right");
             Debug.Log("HERE!");
         }
-
+*/
         // Walking/Looking left/right
         if (rightStickHorizontal > 0.15)
         {
@@ -63,14 +71,52 @@ public class PlayerMovementScript : MonoBehaviour
         if (rightStickHorizontal < -0.15)
         {
             GameObject.FindWithTag("Player").transform.Rotate(Vector3.up, (rightStickHorizontal / 3) * 4);
-        } 
+        }
 
+        checkKeys();
+        checkLook();
+        Debug.Log(Input.GetAxis("Mouse X"));
+    }
 
-        
-        
+    void checkKeys()
+    {
+        if (Input.GetButton("Forward"))
+        {
+            mAnim.SetTrigger("Walk Forward");
+        }
+        else if (Input.GetKey(KeyCode.LeftShift))
+        {
+            mAnim.SetTrigger("Run Forward");
+        }
+        else if (Input.GetButton("Backward"))
+        {
+            mAnim.SetTrigger("Walk Backward");
+        }
+        else if (Input.GetButton("Left"))
+        {
+            mAnim.SetTrigger("Strafe Left");
+        }
+        else if (Input.GetButton("Right"))
+        {
+            mAnim.SetTrigger("Strafe Right");
+        }
+        else
+        {
+            mAnim.SetTrigger("Stop Walking");
+        }
+    }
 
+    void checkLook()
+    {
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = -Input.GetAxis("Mouse Y");
 
+        rotY += mouseX * mouseSensitivity * Time.deltaTime;
+        rotX += mouseY * mouseSensitivity * Time.deltaTime;
 
+        rotX = Mathf.Clamp(rotX, -clampAngle, clampAngle);
 
+        Quaternion localRotation = Quaternion.Euler(rotX, rotY, 0.0f);
+        transform.rotation = localRotation;
     }
 }
